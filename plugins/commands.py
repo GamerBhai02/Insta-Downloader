f6f31c385dffdd2cdcf906c61f8e1af3730eaea6
GamerBhai02 <101631188+GamerBhai02@users.noreply.github.com>
2023-09-21 07:33:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,8 +9,8 @@ async def monitor(Mbot, message):
           
 @Mbot.on_message(filters.command("start") & filters.incoming)
 async def start(Mbot, message):
-          await message.reply(f"Hello 👋👋 {message.from_user.mention()}\n I am A Simple Fastest  Instagram Downloader Bot Currently Supports Reels and Post")
+          await message.reply(f"👋👋 Hello{message.from_user.mention()}\nCurrently, I Am The Easiest And Fastest Instagram Downloader Bot, Supporting Reels.")
           
 @Mbot.on_message(filters.command("help") & filters.incoming)
 async def help(Mbot, message):
-          await message.reply("This is user friendly bot so you can simple send your Instagram reel and post links here:) \n eg: `https://www.instagram.com/reel/CZqWDGODoov/?igshid=MzRlODBiNWFlZA==`\n `post:` `https://www.instagram.com/reel/CuCTtORJbDj/?igshid=MzRlODBiNWFlZA==`")
+          await message.reply("☺ You can easily submit your Instagram reel and post URLs with this user-friendly bot.\n\nExample: \nReel: `https://www.instagram.com/reel/CZqWDGODoov/?igshid=MzRlODBiNWFlZA==`")
